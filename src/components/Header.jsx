f0124deeb1dcb211ee63dc94693fb8341b4f19a4
ninopractio <163866879+pz4pod@users.noreply.github.com>
2024-04-24 18:25:54 +0300

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div>
+                <img></img>
+                <a href="#s">ОЛИВА-МЕД</a>
+                <p>медицинская клиника</p>
+                <a href="#s">Версия для слабовидящих</a>
+                <p>Поиск</p>
+                <a href = "#s"></a>
+                <a href = "#s"></a>
+                <a href = "#s"></a>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+    return(
+        <nav className='nav'>
+            <a>ГЛАВНАЯ</a>
+            <a>О КЛИНИКЕ</a>
+            <a>УСЛУГИ</a>
+            <a>ВРАЧИ</a>
+            <a>ПАЦИЕНТАМ</a>
+            <a>ВАКАНСИИ</a>
+            <a>КОНТАКТЫ</a>
+        </nav>
+    )
+}
+export default Nav;
